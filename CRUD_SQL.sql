2c4ddd9f2fe19f4b2733ad9a857c2e3608e38a88
RecheEduardo <Eduardorechemartins2006@gmail.com>
2024-12-01 01:53:07 -0300

    update database preset

--- CRUD_SQL.sql
+++ CRUD_SQL.sql
@@ -1,6 +1,9 @@
-CREATE DATABASE edudatabase;
+CREATE DATABASE IF NOT EXISTS `edudatabase`;
+USE `edudatabase`;
 
-USE DATABASE edudatabase;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 CREATE TABLE `admins` (
   `id` int(11) NOT NULL,
@@ -10,8 +13,8 @@ CREATE TABLE `admins` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO `admins` (`id`, `nome`, `email`, `senha`) VALUES
-(10, 'Eduardo', 'Eduardo@gmail.com', '123');
-
+(10, 'Eduardo', 'Eduardo@gmail.com', '123'),
+(11, 'Usuário Teste', 'teste@email.com', '1234');
 
 CREATE TABLE `clientes` (
   `id` int(50) NOT NULL,
@@ -27,10 +30,8 @@ INSERT INTO `clientes` (`id`, `nome`, `idade`, `email`, `created_at`) VALUES
 (387, 'Joana', 25, 'Joana@hotmail.com', '2024-11-03 16:09:09'),
 (388, 'Maria Silva', 30, 'maria.silva@example.com', '2024-11-03 18:00:00'),
 (389, 'Carlos Oliveira', 22, 'carlos.oliveira@example.com', '2024-11-03 18:05:00'),
-(390, 'Ana Beatriz', 35, 'ana.beatriz@example.com', '2024-11-03 18:10:00'),
 (391, 'Pedro Almeida', 27, 'pedro.almeida@example.com', '2024-11-03 18:15:00'),
 (392, 'Laura Costa', 29, 'laura.costa@example.com', '2024-11-03 18:20:00');
-
 
 CREATE TABLE `pedidos` (
   `pedido_id` int(11) NOT NULL,
@@ -42,38 +43,28 @@ CREATE TABLE `pedidos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO `pedidos` (`pedido_id`, `data_pedido`, `produto_pedido`, `produto_quantidade`, `endereco_pedido`, `usuario_pedido`) VALUES
-(1, '2024-11-02 22:34:41', 7, 15, 'Rua Iacanga 124', 382),
-(7, '2024-11-03 01:34:41', 1, 2, 'Rua da Alegria, 100', 378),
-(8, '2024-11-03 01:35:00', 2, 1, 'Av. Brasil, 500', 379),
-(9, '2024-11-03 01:36:00', 3, 3, 'Rua das Flores, 250', 382),
-(10, '2024-11-03 01:37:00', 4, 1, 'Rua do Sol, 10', 385),
-(11, '2024-11-03 01:38:00', 5, 4, 'Praça da Liberdade, 30', 378),
-(12, '2024-11-03 16:28:37', 9, 25, 'Rua Iacanga, 124', 392),
-(13, '2024-11-03 16:31:03', 13, 12, 'Rua da Liberdade, 1', 389),
-(14, '2024-11-03 16:33:48', 12, 16, 'Rua do Canal, 345', 388),
-(15, '2024-11-03 16:39:57', 11, 45, 'Praça do Imperador, 1243', 390);
-
+(16, '2024-11-30 23:30:16', 16, 6, 'Rua Iacanga 124, Vila Maria, São Paulo - SP', 389),
+(17, '2024-11-30 23:35:26', 17, 2, 'Rua Maria de Lourdes, 133, Vila Gustavo, São Paulo - SP', 388),
+(18, '2024-12-01 02:46:00', 15, 1, 'Avenida Paulo Faccini, 807, Centro, Guarulhos - SP', 392);
 
 CREATE TABLE `produtos` (
   `produto_id` int(11) NOT NULL,
   `produto_nome` varchar(255) NOT NULL,
   `produto_preco` float NOT NULL,
   `produto_descricao` varchar(255) NOT NULL,
+  `produto_imagem` varchar(5000) NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
   `updatedAt` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `produtos` (`produto_id`, `produto_nome`, `produto_preco`, `produto_descricao`, `createdAt`, `updatedAt`) VALUES
-(9, 'Carrinho Hot Wheels', 79.99, 'Carrinho Hot Wheels em miniatura', '2024-11-03 01:29:04', '2024-11-03 16:36:37'),
-(10, 'Pista de Corrida', 150, 'Pista de corrida Hot Wheels com curvas', '2024-11-03 01:30:00', '2024-11-03 16:20:00'),
-(11, 'Conjunto de Blocos de Montar', 120, 'Conjunto com 300 blocos de montar', '2024-11-03 01:31:00', '2024-11-03 16:30:00'),
-(12, 'Boneco de Ação', 45, 'Boneco de ação de super-herói', '2024-11-03 01:32:00', '2024-11-03 16:40:00'),
-(13, 'Jogo de Tabuleiro', 99, 'Jogo de tabuleiro divertido para família', '2024-11-03 01:33:00', '2024-11-03 16:50:00');
-
+INSERT INTO `produtos` (`produto_id`, `produto_nome`, `produto_preco`, `produto_descricao`, `produto_imagem`, `createdAt`, `updatedAt`) VALUES
+(15, 'Chanel Nº5 Eau de Parfum', 925.5, 'Um perfume floral icônico com notas de aldeídos e ylang-ylang.', 'https://www.chanel.com/images//t_one//w_0.51,h_0.51,c_crop/q_auto:good,f_autoplus,fl_lossy,dpr_1.1/w_1240/n-5-eau-de-parfum-spray-3-4fl-oz--packshot-default-125530-9539148742686.jpg', '2024-11-30 23:23:54', '2024-12-01 02:47:03'),
+(16, 'Dior Sauvage Eau de Toilette', 1109.5, 'Uma fragrância fresca com notas de bergamota e ambroxan.', 'https://epocacosmeticos.vteximg.com.br/arquivos/ids/626106/3348901728836-3348901728829--2-.jpg?v=638633240319130000', '2024-11-30 23:23:54', '2024-12-01 03:14:02'),
+(17, 'Lancôme La Vie Est Belle', 569.9, 'Uma mistura doce de íris, patchouli e baunilha.', 'https://epocacosmeticos.vteximg.com.br/arquivos/ids/577799/3605532612690-30ml--2-.jpg?v=638349532597930000', '2024-11-30 23:23:54', '2024-12-01 02:47:26'),
+(18, 'Gucci Bloom Eau de Parfum', 768.8, 'Um perfume floral intenso com notas de jasmim e tuberosa.', 'https://epocacosmeticos.vteximg.com.br/arquivos/ids/546127-800-800/gucci-bloom.jpg?v=638179395546030000', '2024-11-30 23:23:54', '2024-12-01 03:14:18');
 
 ALTER TABLE `admins`
   ADD PRIMARY KEY (`id`);
-
 
 ALTER TABLE `clientes`
   ADD PRIMARY KEY (`id`);
@@ -81,23 +72,19 @@ ALTER TABLE `clientes`
 ALTER TABLE `pedidos`
   ADD PRIMARY KEY (`pedido_id`);
 
-
 ALTER TABLE `produtos`
   ADD PRIMARY KEY (`produto_id`);
 
-
 ALTER TABLE `admins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 ALTER TABLE `clientes`
   MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=393;
 
-
 ALTER TABLE `pedidos`
-  MODIFY `pedido_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-
+  MODIFY `pedido_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 ALTER TABLE `produtos`
-  MODIFY `produto_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `produto_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
 COMMIT;
